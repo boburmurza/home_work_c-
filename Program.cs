9020ad10e4646202135eac67a9cdbb210896b649
Bobur <boryamur56@gmail.com>
2022-11-30 11:24:02 +0300

    решили №3 задачу

--- Program.cs
+++ Program.cs
@@ -21,17 +21,29 @@
 // 2, 3, 7 -> 7
 // 44 5 78 -> 78
 // 22 3 9 -> 22
-Console.Write("введите число  a = : ");
-int number_a = Convert.ToInt32( Console.ReadLine() ); 
-Console.Write("введите число  b = : ");
-int number_b = Convert.ToInt32( Console.ReadLine() ); 
-Console.Write("введите число  c = : ");
-int number_c = Convert.ToInt32( Console.ReadLine() ); 
-if (number_a>number_b& number_a>number_c)
-    Console.WriteLine( " максмимальное  число = : "+number_a);   
-else if  (number_b>number_c) 
-    Console.WriteLine( " максмимальное  число = : "+number_b );
-else if  ( number_b<number_c) 
-      Console.WriteLine (" максмимальное  число = : "+number_c) ;
-else
-    Console.WriteLine(  "error "); 
+// Console.Write("введите число  a = : ");
+// int number_a = Convert.ToInt32( Console.ReadLine() ); 
+// Console.Write("введите число  b = : ");
+// int number_b = Convert.ToInt32( Console.ReadLine() ); 
+// Console.Write("введите число  c = : ");
+// int number_c = Convert.ToInt32( Console.ReadLine() ); 
+// if (number_a>number_b& number_a>number_c)
+//     Console.WriteLine( " максмимальное  число = : "+number_a);   
+// else if  (number_b>number_c) 
+//     Console.WriteLine( " максмимальное  число = : "+number_b );
+// else if  ( number_b<number_c) 
+//       Console.WriteLine (" максмимальное  число = : "+number_c) ;
+// else
+//     Console.WriteLine(  "error "); 
+// задача №  3 Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+ Console.Write("введите число  number = : ");
+int number = Convert.ToInt32( Console.ReadLine() ); 
+if (number%2==0)
+     Console.Write(number+"  --→ да ");
+else 
+     Console.Write(number+" --→ нет ");
