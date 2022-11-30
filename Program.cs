55116915a82916b19bbbc57c9b90654ad2044ca0
Bobur <boryamur56@gmail.com>
2022-11-30 11:09:50 +0300

    решили №2 задачу

--- Program.cs
+++ Program.cs
@@ -4,13 +4,34 @@
 // a = -9 b = -3 -> max = -3
 
 
+// Console.Write("введите число  a = : ");
+// int number_a = Convert.ToInt32( Console.ReadLine() ); 
+// Console.Write("введите число  b = : ");
+// int number_b = Convert.ToInt32( Console.ReadLine() ); 
+// if (number_a>number_b){
+//     Console.WriteLine( " максмимальное  число = : "+number_a);}
+// else if  ( number_b>number_a) {
+//     Console.WriteLine (" максмимальное  число = : "+number_b );
+// }
+ 
+
+
+// задача №  2 Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
 Console.Write("введите число  a = : ");
 int number_a = Convert.ToInt32( Console.ReadLine() ); 
 Console.Write("введите число  b = : ");
 int number_b = Convert.ToInt32( Console.ReadLine() ); 
-if (number_a>number_b){
-    Console.WriteLine( " максмимальное  число = : "+number_a);}
-else if  ( number_b>number_a) {
-    Console.WriteLine (" максмимальное  число = : "+number_b );
-}
-
+Console.Write("введите число  c = : ");
+int number_c = Convert.ToInt32( Console.ReadLine() ); 
+if (number_a>number_b& number_a>number_c)
+    Console.WriteLine( " максмимальное  число = : "+number_a);   
+else if  (number_b>number_c) 
+    Console.WriteLine( " максмимальное  число = : "+number_b );
+else if  ( number_b<number_c) 
+      Console.WriteLine (" максмимальное  число = : "+number_c) ;
+else
+    Console.WriteLine(  "error "); 
