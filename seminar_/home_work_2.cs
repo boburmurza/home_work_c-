eba997c650e843b7a8b4a318a0ef2816e80fbe83
Bobur <boryamur56@gmail.com>
2022-12-03 14:41:28 +0300

    решили первую задачу

---
+++ seminar_/home_work_2.cs
@@ -0,0 +1,17 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+// int Test(int number){
+
+//  Console.WriteLine( " вы ввели это число := "+number);
+//  int hund = number/100;
+//  int ten = number/10%10;
+//     return ( ten);
+// }
+// Console.WriteLine("введите трехзначное число ");
+// int number = Convert.ToInt32(Console.ReadLine());
+//  int res = Test(number);
+
+//  Console.WriteLine( "второе число :="+ res);
